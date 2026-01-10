e4bd277dbd2ea9e3abf2221abc2419e9435e49a2
NoName24 <146374556+gNoName24@users.noreply.github.com>
2026-01-10 19:14:45 +0500

    Init

---
+++ include/noname24/blockybinary.hpp
@@ -0,0 +1,13 @@
+/* ./include/noname24/blockybinary.hpp
+ * NoName24 - 2026
+ */
+#ifndef NONAME24_BLOCKYBINARY_HPP
+#define NONAME24_BLOCKYBINARY_HPP
+
+namespace NoName24 {
+    namespace BlockyBinary {
+
+    };
+};
+
+#endif // NONAME24_BLOCKYBINARY_HPP
---
+++ src/noname24/blockybinary.cpp
@@ -0,0 +1,10 @@
+/* ./src/noname24/blockybinary.hpp
+ * NoName24 - 2026
+ */
+#include <noname24/blockybinary.hpp>
+
+namespace NoName24 {
+    namespace BlockyBinary {
+
+    };
+};
